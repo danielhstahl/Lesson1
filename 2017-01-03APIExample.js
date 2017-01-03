d675b9f9993b9bf2dcf9504d98c7aa49aaa4bc82
Thomas Nguyen <V7XH3@regions.com>
2017-01-03 08:36:41 -0600

    Thomas's Submission

--- 2017-01-03APIExample.js
+++ 2017-01-03APIExample.js
@@ -1,12 +1,11 @@
 const https = require('https');
 const http = require('http');
+var i = 'http://jsonplaceholder.typicode.com/users?id=1'
 
-http.get('http://jsonplaceholder.typicode.com/posts', (res) => {
-
+http.get(i, (res) => {
   res.on('data', (d) => {
     process.stdout.write(d);
   });
-
 }).on('error', (e) => {
   console.error(e);
 });
