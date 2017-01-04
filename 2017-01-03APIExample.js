7cf10262a23bc50e6e4809273f78c4cdd841a622
Dwight A. Young <c5mg9@regions.com>
2017-01-04 13:40:09 -0600

    first commit

--- 2017-01-03APIExample.js
+++ 2017-01-03APIExample.js
@@ -1,12 +1,12 @@
 const https = require('https');
 const http = require('http');
+var i = 'http://jsonplaceholder.typicode.com/users?id=3';
 
-http.get('http://jsonplaceholder.typicode.com/posts', (res) => {
 
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
-
+http.get(i, (res) => {
+	res.on('data', (d) => {
+		process.stdout.write(d);
+	});
 }).on('error', (e) => {
-  console.error(e);
+	console.error(e);
 });
