b02efeb73311d1a9d01d1398b9418b9ecebc3fbe
Aaron T. Bridgers <H7JV9@regions.com>
2017-01-04 16:17:21 -0600

    Aaron Lesson1

--- 2017-01-03APIExample.js
+++ 2017-01-03APIExample.js
@@ -1,7 +1,7 @@
 const https = require('https');
 const http = require('http');
 
-http.get('http://jsonplaceholder.typicode.com/posts', (res) => {
+https.get('https://api.stlouisfed.org/fred/series/observations?series_id=DSWP1&api_key=6b75e4bc06a6ed991a7a9cc64d70c3fa&file_type=json', (res) => {
 
   res.on('data', (d) => {
     process.stdout.write(d);
